9ac2416e0c32c43c529c872667075a81c48d29fe
Ilya Kirillov <ilya.kirillov@jetbrains.com>
2020-11-17 14:09:16 +0300

    WIP TRANSFORMER PROVIDER

--- idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/FirTransformerProvider.kt
+++ idea/idea-frontend-fir/idea-fir-low-level-api/src/org/jetbrains/kotlin/idea/fir/low/level/api/FirTransformerProvider.kt
@@ -5,32 +5,18 @@
 
 package org.jetbrains.kotlin.idea.fir.low.level.api
 
-import com.google.common.collect.MapMaker
 import org.jetbrains.kotlin.fir.FirSession
-import org.jetbrains.kotlin.fir.FirSessionComponent
 import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
 import org.jetbrains.kotlin.fir.resolve.ScopeSession
 import org.jetbrains.kotlin.fir.resolve.transformers.FirTransformerBasedResolveProcessor
 import org.jetbrains.kotlin.fir.resolve.transformers.createTransformerBasedProcessorByPhase
-import java.util.*
+
 
 class FirTransformerProvider {
-    private val scopeSession = MapMaker().weakKeys().makeMap<FirSession, ScopeSession>()
-    private val transformers = MapMaker().weakKeys().makeMap<FirSession, EnumMap<FirResolvePhase, FirTransformerBasedResolveProcessor>>()
+    fun getTransformerForPhase(session: FirSession, phase: FirResolvePhase): FirTransformerBasedResolveProcessor {
+        val scopesSession = getScopeSession(session)
+        return phase.createTransformerBasedProcessorByPhase(session, scopesSession)
+    }
 
-
-    fun getTransformerForPhase(session: FirSession, phase: FirResolvePhase): FirTransformerBasedResolveProcessor =
-        transformers.computeIfAbsent(session) {
-            val scopesSession = getScopeSession(session)
-            EnumMap<FirResolvePhase, FirTransformerBasedResolveProcessor>(FirResolvePhase::class.java).apply {
-                FirResolvePhase.values().forEach { resolvePhase ->
-                    if (resolvePhase != FirResolvePhase.RAW_FIR) {
-                        put(resolvePhase, resolvePhase.createTransformerBasedProcessorByPhase(session, scopesSession))
-                    }
-                }
-            }
-        }.getValue(phase)
-
-    fun getScopeSession(session: FirSession): ScopeSession =
-        scopeSession.getOrPut(session) { ScopeSession() }
+    fun getScopeSession(session: FirSession): ScopeSession = ScopeSession()
 }
